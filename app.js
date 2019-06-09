0179431514e4cf59f876e3d6dbd19b227db4e738
Maryanne Heinbaugh <23heinbau@gmail.com>
2019-06-09 07:42:07 -0700

    created file structure and html format

---
+++ app.js
@@ -0,0 +1,12 @@
+// API key QDbQHB76EXbeaDedJzwuuVU3b6ldCiRj
+// var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5");
+
+// Initial array of superheros
+var topics = ["Batman", "Ironman","Wonder Woman","Wasp"]
+
+// Function for placing the JSON content for each button into the div
+function displaySuperheroGifs();
+
+
+// Function for displaying the buttons
+function renderButtons()
